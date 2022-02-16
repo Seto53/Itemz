2b6003d79d0269ff938819bd213f9f5a281a6f1b
Mohamed <60052502+Setomaster@users.noreply.github.com>
2022-02-15 21:44:54 -0500

    Add tailwind and bootstrap, update header and logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,35 +1,169 @@
 import React from "react";
-import profile from '../docs/assets/profile_picture.png';
-import gold from '../docs/assets/gold.png';
-import logo from '../docs/assets/logo_with_name.png';
-import  '../docs/styles/header.css';
-import  '../docs/styles/root.css';
+import '../docs/styles/header.css';
+import '../docs/styles/root.css';
+import {Disclosure, Menu} from '@headlessui/react'
+import {BellIcon, MenuIcon, XIcon} from '@heroicons/react/outline'
+
+const navigation = [
+    {name: 'Explore', href: './explore', current: true},
+    {name: 'Home', href: './Home', current: false},
+    {name: 'Settings', href: './settings', current: false},
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function Header({element}) {
     return (
         <div>
             <header className="App-header">
                 <div className="header-class">
-                    <a href="/"> <img alt="logo" className="logo-image" src={logo}/> </a>
-                    <div className="navigation-container w-container">
-                        <form action="" className="search-3">
-                            <label htmlFor="search"> </label>
-                            <input className="search-input-2 w-input" id="search"
-                                   maxLength="256" name="query"
-                                   placeholder="Search items and drops"
-                                   required=""
-                                   type="search"/>
-                            <input className="search-button-2 w-button" type="submit" value="Search"/>
-                        </form>
-                    </div>
-                    <div className="header-options">
-                        <a href="/"> Home </a>
-                        <a href="/explore"> Explore </a>
-                        <a href="/settings"> Settings </a>
-                        <h3>67</h3>
-                        <a href="/settings"> <img alt="gold" className="tiny-profile-p" src={gold}/></a>
-                        <a href="/profile"> <img alt="profile" className="tiny-profile-p" src={profile}/></a>
-                    </div>
+
+                    <Disclosure as="nav" className="bg-gray-800">
+                        {({open}) => (
+                            <>
+                                <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                                    <div className="relative flex items-center justify-between h-16">
+                                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                            {/* Mobile menu button*/}
+                                            <Disclosure.Button
+                                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                                <span className="sr-only">Open main menu</span>
+                                                {open ? (
+                                                    <XIcon className="block h-6 w-6" aria-hidden="true"/>
+                                                ) : (
+                                                    <MenuIcon className="block h-6 w-6" aria-hidden="true"/>
+                                                )}
+                                            </Disclosure.Button>
+                                        </div>
+                                        <div
+                                            className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                                            <a href={'./Home'}>
+                                                <div className="flex-shrink-0 flex items-center">
+                                                    <img
+                                                        className="block lg:hidden h-10 w-auto"
+                                                        src={require('../docs/assets/logo.png')}
+                                                        alt="ItemZ"
+                                                    />
+                                                    <img
+                                                        className="hidden lg:block h-10 w-auto"
+                                                        src={require('../docs/assets/logo_with_name.png')}
+                                                        alt="ItemZ"
+                                                    />
+                                                </div>
+                                            </a>
+                                            <div className="hidden sm:block sm:ml-6">
+                                                <div className="flex space-x-4">
+                                                    {navigation.map((item) => (
+                                                        <a
+                                                            key={item.name}
+                                                            href={item.href}
+                                                            className={classNames(
+                                                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                                'px-3 py-2 rounded-md text-sm font-medium'
+                                                            )}
+                                                            aria-current={item.current ? 'page' : undefined}
+                                                        >
+                                                            {item.name}
+                                                        </a>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div
+                                            className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                            <button
+                                                type="button"
+                                                className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                            >
+                                                <span className="sr-only">View notifications</span>
+                                                <BellIcon className="h-6 w-6" aria-hidden="true"/>
+                                            </button>
+
+                                            {/* Profile dropdown */}
+                                            <Menu as="div" className="ml-3 relative">
+                                                <div>
+                                                    <a href={'./profile'}>
+                                                            <span className="sr-only">Open user menu</span>
+                                                            <img
+                                                                className="h-8 w-8 rounded-full"
+                                                                src={require('../docs/assets/profile_picture.png')}
+                                                                alt="profile picture"
+                                                            />
+                                                        </a>
+                                                    {/*</div>*/}
+                                                    {/*<Transition*/}
+                                                    {/*    as={Fragment}*/}
+                                                    {/*    enter="transition ease-out duration-100"*/}
+                                                    {/*    enterFrom="transform opacity-0 scale-95"*/}
+                                                    {/*    enterTo="transform opacity-100 scale-100"*/}
+                                                    {/*    leave="transition ease-in duration-75"*/}
+                                                    {/*    leaveFrom="transform opacity-100 scale-100"*/}
+                                                    {/*    leaveTo="transform opacity-0 scale-95"*/}
+                                                    {/*>*/}
+                                                    {/*    <Menu.Items*/}
+                                                    {/*        className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">*/}
+                                                    {/*        <Menu.Item>*/}
+                                                    {/*            {({active}) => (*/}
+                                                    {/*                <a*/}
+                                                    {/*                    href="#"*/}
+                                                    {/*                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}*/}
+                                                    {/*                >*/}
+                                                    {/*                    Your Profile*/}
+                                                    {/*                </a>*/}
+                                                    {/*            )}*/}
+                                                    {/*        </Menu.Item>*/}
+                                                    {/*        <Menu.Item>*/}
+                                                    {/*            {({active}) => (*/}
+                                                    {/*                <a*/}
+                                                    {/*                    href="#"*/}
+                                                    {/*                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}*/}
+                                                    {/*                >*/}
+                                                    {/*                    Settings*/}
+                                                    {/*                </a>*/}
+                                                    {/*            )}*/}
+                                                    {/*        </Menu.Item>*/}
+                                                    {/*        <Menu.Item>*/}
+                                                    {/*            {({active}) => (*/}
+                                                    {/*                <a*/}
+                                                    {/*                    href="#"*/}
+                                                    {/*                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}*/}
+                                                    {/*                >*/}
+                                                    {/*                    Sign out*/}
+                                                    {/*                </a>*/}
+                                                    {/*            )}*/}
+                                                    {/*        </Menu.Item>*/}
+                                                    {/*    </Menu.Items>*/}
+                                                    {/*</Transition>*/}
+                                                </div>
+                                            </Menu>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <Disclosure.Panel className="sm:hidden">
+                                    <div className="px-2 pt-2 pb-3 space-y-1">
+                                        {navigation.map((item) => (
+                                            <Disclosure.Button
+                                                key={item.name}
+                                                as="a"
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'block px-3 py-2 rounded-md text-base font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </Disclosure.Button>
+                                        ))}
+                                    </div>
+                                </Disclosure.Panel>
+                            </>
+                        )}
+                    </Disclosure>
+
                 </div>
             </header>
             {element}
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import  '../docs/styles/root.css';
-import  '../docs/styles/drop.css';
-import  '../docs/styles/button.css';
-import  '../docs/styles/hero.css';
-import  '../docs/styles/countdown.css';
+import '../docs/styles/root.css';
+import '../docs/styles/drop.css';
+import '../docs/styles/button.css';
+import '../docs/styles/hero.css';
+import '../docs/styles/countdown.css';
 import cover from '../docs/assets/cover.png';
 import element from '../docs/assets/collectibles/element.png';
 import tempCollectible from '../docs/assets/temp-digital-collectibles.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import CountDown from './CountDown';
 
 export default function Home() {
@@ -19,27 +20,33 @@ export default function Home() {
                 <img alt="cover" className="cover" src={cover}/>
             </div>
 
-            <div className="outer" id="wrapper">
-                <div className="outer" id="hero-details">
-                    <h1 className="">Discover, collect, and share your digital collectibles</h1>
-                    <p className="">Quickly design and customize responsive mobile-first sites with Bootstrap, the
-                        world’s
-                        most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid
-                        system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                    <div className="button-container">
-                        <a className="button" href={"/explore"}>
-                            <p>Explore</p>
-                        </a>
-                        <a className="button" href={"/profile"}>
-                            <p>Profile</p>
-                        </a>
+            <div className="container col-xxl-8 px-4 py-5">
+                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                    <div className="col-10 col-sm-8 col-lg-6">
+                        <img alt="Digital Collectibles" className="d-block mx-lg-auto img-fluid drop" height="510"
+                             loading="lazy"
+                             src={tempCollectible} width="680"/>
+                    </div>
+                    <div className="col-lg-6">
+                        <h1 className="display-5 fw-bold lh-1 mb-3">Discover, collect, and share your digital
+                            collectibles</h1>
+                        <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap,
+                            the world’s
+                            most popular front-end open source toolkit, featuring Sass variables and mixins, responsive
+                            grid
+                            system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                            <a href="/explore">
+                                <button className="btn btn-primary btn-lg px-4 me-md-2" type="button">Explore</button>
+                            </a>
+                            <a href="/profile">
+                                <button className="btn btn-outline-secondary btn-lg px-4" type="button">Profile</button>
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <div className="outer">
-                    <img alt="Digital Collectibles" className="drop" loading="lazy"
-                         src={tempCollectible} width="680"/>
-                </div>
             </div>
+
             <CountDown drop={newDrop}/>
             <div className="drop-header">
                 <h1>Recent Drops</h1>
