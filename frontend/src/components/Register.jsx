1c05edda92411e1b5af87de2308b4f8d57994933
Mohamed <60052502+Setomaster@users.noreply.github.com>
2022-02-18 19:53:30 -0500

    Update login and register

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,46 +1,102 @@
 import React from "react";
-import  '../docs/styles/root.css';
-import  '../docs/styles/login.css';
-import { useNavigate } from "react-router-dom";
+import '../docs/styles/root.css';
+import {useNavigate} from "react-router-dom";
+import {LockClosedIcon} from "@heroicons/react/solid";
 
 export default function Login() {
     const navigate = useNavigate();
 
     function handleSuccess() {
-        navigate('/', { replace: true });
+        navigate('/', {replace: true});
     }
 
     return (
-        <div className="login-form">
-            <form>
-                <h1>Register</h1>
-                <div className="login-container">
-                    <div className="input-field">
-                        <label>
-                            <input placeholder="Username" type="text"/>
-                        </label>
+        <>
+            <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-md w-full space-y-8">
+                    <div>
+                        <img
+                            className="mx-auto h-12 w-auto"
+                            src={require('../docs/assets/logo.png')}
+                            alt="Workflow"
+                        />
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create your ItemZ account</h2>
                     </div>
-                    <div className="input-field">
-                        <label>
-                            <input placeholder="Email" type="email"/>
-                        </label>
-                    </div>
-                    <div className="input-field">
-                        <label>
-                            <input autoComplete="new-password" placeholder="Password" type="password"/>
-                        </label>
-                    </div>
-                    <div className="input-field">
-                        <label>
-                            <input autoComplete="new-password" placeholder="re-enter your password" type="password"/>
-                        </label>
-                    </div>
+                    <form className="mt-8 space-y-6" action="#" method="POST">
+                        <input type="hidden" name="remember" defaultValue="true"/>
+                        <div className="rounded-md shadow-sm -space-y-px">
+                            <div>
+                                <label htmlFor="username" className="sr-only">
+                                    Username
+                                </label>
+                                <input
+                                    id="username"
+                                    name="username"
+                                    type="username"
+                                    autoComplete="current-password"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Username"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Email address
+                                </label>
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Email address"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="sr-only">
+                                    Password
+                                </label>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password-re" className="sr-only">
+                                    Re-enter your password
+                                </label>
+                                <input
+                                    id="password-re"
+                                    name="password-re"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Re-enter your password"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="bg-indigo-600 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 "
+                            >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true"/>
+                </span>
+                                Sign up
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <p>By registering, you agree to the Terms and Conditions.</p>
-                <div className="action">
-                    <button id="main-button" onClick={handleSuccess}>Register</button>
-                </div>
-            </form>
-        </div>
+            </div>
+        </>
     );
 }
