83359bb56926a6c12f045966dcd472b156d3ebf3
Rocio Ferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2022-02-21 15:17:13 -0500

    react test running

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+//Dummy first test
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Discover, collect, and share your digital collectibles/i);
   expect(linkElement).toBeInTheDocument();
 });
