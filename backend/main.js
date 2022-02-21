7d1d796fbee03e80c2e7557940b8def6d8f68c07
Rocio Ferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2022-02-21 13:35:11 -0500

    add backend testing framework

--- backend/main.js
+++ backend/main.js
@@ -1,33 +1,3 @@
-// const http = require("http");
-// const { Client } = require('pg');
-//
-// http.createServer(function (request, response) {
-//     // Send the HTTP header
-//     // HTTP Status: 200 : OK
-//     // Content Type: text/plain
-//     response.writeHead(200, {'Content-Type': 'text/plain'});
-//
-//     // Send the response body as "Hello World"
-//     response.end('Hello World\n');
-// }).listen(8081);
-//
-// // Console will print the message
-// console.log('Server running at http://127.0.0.1:8081/');
-//
-//
-// const client = new Client({
-//     user: process.env.PGUSER,
-//     host: process.env.PGHOST,
-//     database: process.env.PGDATABASE,
-//     password: process.env.PGPASSWORD,
-//     port: process.env.PGPORT,
-// })
-//
-// client.connect()
-// client.query('SELECT $1::text as message', ['Connected to Data Base!'], (err, res) => {
-//     console.log(err ? err.stack : res.rows[0].message) // Connected to Data Base!
-//     client.end()
-// })
 require('dotenv').config();
 const express = require('express')
 const bodyParser = require('body-parser')
